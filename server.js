21c924cd48107a4995e4825526e8fd62c0108c4c
fnedarb <braden.fink49@gmail.com>
2021-10-11 14:52:38 -0400

    added express dependency and view engine

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-// === Global Variables ===
+// === External Modules === 
+const express = require('express');
+
+/ === Global Variables ===
 const PORT = 4000;
+
+// === Express Dependency ===
+const app = express();
+
+// === Configs === 
+app.set('view engine', 'ejs');
 
 // === Listener Listening ===
 app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT}`));
