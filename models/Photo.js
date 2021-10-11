4d28d767782ca133a4d7f0e58cf080f5b4f176bc
fnedarb <braden.fink49@gmail.com>
2021-10-11 18:30:00 -0400

    added photo view, simple index page, and a route to display it

--- models/Photo.js
+++ models/Photo.js
@@ -4,10 +4,10 @@ const photoSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'photo name cannot be empty'],
-    }
+    },
     description: {
         type: String,
-    }
+    },
     image: {
         type: String,
         required : [true, 'image cannot be empty'],
