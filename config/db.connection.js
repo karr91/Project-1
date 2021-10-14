c829a2334c1347659dc39a5cd37042662cb73397
fnedarb <braden.fink49@gmail.com>
2021-10-14 19:13:08 -0400

    changed db

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/PhotoExpress2';
+const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/PhotoExpress';
 
 mongoose.connect(connectionStr, {
     useNewUrlParser: true,
