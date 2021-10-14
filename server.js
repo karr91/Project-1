b73ae2e90941435b850b7a4d585e5624d813d36c
fnedarb <braden.fink49@gmail.com>
2021-10-14 19:08:40 -0400

    changes to photo controller to facilitate user verification

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 4000;
 const controllers = require("./controllers");
 const navLinks = require("./navLinks/navlinks.js");
 
+
 // === Express Dependency ===
 const app = express();
 
